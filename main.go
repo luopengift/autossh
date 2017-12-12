5200ea3fe20be72a92505a2f86a7926685f60648
luopengift <870148195@qq.com>
2017-12-12 20:01:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,46 +1,109 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"github.com/luopengift/types"
 	"github.com/luopengift/golibs/logger"
 	"github.com/luopengift/golibs/ssh"
+	"github.com/luopengift/types"
 	"strconv"
 	"strings"
 )
 
 var log *logger.Logger
 
-type Config struct {
-    Servers []*ssh.Endpoint `yaml:"servers"`
+type ServerList struct {
+	Global  *ssh.Endpoint   `yaml:"global"`
+	Servers []*ssh.Endpoint `yaml:"servers"`
+	result  []*ssh.Endpoint
+}
+
+func (s *ServerList) Println() {
+	fmt.Println(fmt.Sprintf("序号\t名称\tIP\t\t端口"))
+	for index, endpoint := range s.result {
+		item := fmt.Sprintf("%v\t%v\t%v\t\t%v", index, endpoint.Name, endpoint.Ip, endpoint.Port)
+		fmt.Println(item)
+	}
+}
+
+func (s *ServerList) Reset() []*ssh.Endpoint {
+	s.result = s.Servers
+	return s.result
+}
+
+func (s *ServerList) Match(match string) []*ssh.Endpoint {
+	result := []*ssh.Endpoint{}
+	for index, endpoint := range s.result {
+		if match == strconv.Itoa(index) || match == endpoint.Name || match == endpoint.Host || match == endpoint.Ip {
+			result = append(result, endpoint)
+		}
+	}
+	s.result = result
+	return s.result
+}
+
+func (s *ServerList) Search(search string) []*ssh.Endpoint {
+	result := []*ssh.Endpoint{}
+	for _, endpoint := range s.result {
+		if strings.Contains(endpoint.Name, search) || strings.Contains(endpoint.Host, search) || strings.Contains(endpoint.Ip, search) {
+			result = append(result, endpoint)
+		}
+	}
+	s.result = result
+	return s.result
+}
+
+func recv_console() string {
+	input := ""
+	n, err := fmt.Scanln(&input)
+	if err != nil {
+		fmt.Println("scan error:", err)
+	}
+	fmt.Println("scan len", n)
+	return input
+}
+
+func ScanLine() string {
+	var c byte
+	var err error
+	var b []byte
+	for err == nil {
+		_, err = fmt.Scanf("%c", &c)
+
+		if c != '\n' {
+			b = append(b, c)
+		} else {
+			break
+		}
+	}
+	return string(b)
 }
 
 func main() {
 	logger.SetPrefix("")
 	logger.Info("Autossh...")
 	err := Exec()
-	fmt.Println(err)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 func Exec() error {
 
-    fd, err := os.OpenFile("log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-    if err != nil {
-        return err
-    }
-    log = logger.NewLogger("2006/01/02 15:04:05.000", logger.DEBUG,fd)
-    conf := &Config{}
-    err = types.ParseConfigFile("autossh.yml", conf)
-    if err != nil {
-        return err
-    }
+	serverList := &ServerList{}
+	err := types.ParseConfigFile("autossh.yml", serverList)
+	if err != nil {
+		return err
+	}
 
-	endpoints := conf.Servers
-	input := "" //屏幕输入
+	serverList.Reset()
+	input := ""
 	for {
-		Println(endpoints)
+
+		serverList.Println()
+		fmt.Println("")
 		fmt.Printf("输入需要登录的服务器: ")
-		fmt.Scanln(&input)
+		input = recv_console()
+		fmt.Println("input: ", "|"+input+"|")
+
 		switch input {
 		case "q", "quit", "exit":
 			fmt.Println("exit...")
@@ -48,57 +111,31 @@ func Exec() error {
 		case "h", "help":
 			fmt.Println("help")
 		default:
+			fmt.Println("default")
 			if strings.HasPrefix(input, "/") {
-				result := Search(endpoints, strings.Trim(input, "/"))
+				result := serverList.Search(strings.Trim(input, "/"))
 				switch len(result) {
 				case 0:
-					result = endpoints
+					serverList.Reset()
 				case 1:
-					return result[0].StartTerminal()
+					err = result[0].StartTerminal()
+					fmt.Println("close.", err)
+					serverList.Reset()
 				}
-				endpoints = result
 			} else if false {
 				continue
 			} else {
-				result := Match(endpoints, strings.Trim(input, " "))
+				result := serverList.Match(strings.Trim(input, " "))
 				switch len(result) {
 				case 0:
-					result = endpoints
+					serverList.Reset()
 				case 1:
-					return result[0].StartTerminal()
+					result[0].StartTerminal()
+					fmt.Println("close.", err)
+					serverList.Reset()
 				}
-				endpoints = result
 			}
 		}
 	}
 	return nil
-}
-
-func Println(servers []*ssh.Endpoint) {
-	fmt.Println(fmt.Sprintf("序号\t名称\tIP\t\t端口"))
-	for index, endpoint := range servers {
-		item := fmt.Sprintf("%v\t%v\t%v\t\t%v", index, endpoint.Name, endpoint.Ip, endpoint.Port)
-		fmt.Println(item)
-	}
-}
-
-func Match(servers []*ssh.Endpoint, match string) []*ssh.Endpoint {
-	result := []*ssh.Endpoint{}
-	for index, endpoint := range servers {
-		if match == strconv.Itoa(index) || match == endpoint.Host || match == endpoint.Ip {
-			result = append(result, endpoint)
-		}
-	}
-	return result
-
-}
-
-func Search(servers []*ssh.Endpoint, search string) []*ssh.Endpoint {
-	result := []*ssh.Endpoint{}
-	for _, endpoint := range servers {
-		if strings.Contains(endpoint.Host, search) || strings.Contains(endpoint.Ip, search) {
-			result = append(result, endpoint)
-		}
-	}
-	return result
 }
